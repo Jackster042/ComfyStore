23350c2ae5f1b417b53b3ff18dc6e3a96f11a007
Nemanja Stojanovic <feemail042@gmail.com>
2024-10-16 22:08:31 +0200

    pages folder structure setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
 import React from "react";
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from "./pages";
 
 const App = () => {
   return (
