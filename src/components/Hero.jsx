c974c8d20ed3e810ffc9ae0461d73cdfd3ead672
Nemanja Stojanovic <feemail042@gmail.com>
2024-10-18 00:42:30 +0200

    axios custom instance, thunder client basic setup

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import hero2 from "../assets/hero2.webp";
 import hero3 from "../assets/hero3.webp";
 import hero4 from "../assets/hero4.webp";
 
-const carouselImages = [hero1, hero2, hero3, hero4];
+// const carouselImages = [hero1, hero2, hero3, hero4];
 
 const Hero = () => {
   return (
@@ -17,8 +17,9 @@ const Hero = () => {
           We are changing the way people shop
         </h1>
         <p className="mt-8 max-w-xl text-lg leading-8">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
-          voluptatem numquam impedit maxime harum atque?
+          Shopping with us is easy and hassle-free. Our user-friendly website
+          allows you to browse our extensive collection, place orders, and
+          arrange delivery—all from the comfort of your home.
         </p>
         <div className="mt-10">
           <Link to="/products" className="btn btn-primary capitalize">
@@ -27,7 +28,7 @@ const Hero = () => {
         </div>
       </div>
       {/* RIGHT SIDE */}
-      <div className="hidden  h-[28rem] lg:carousel carousel-center   p-4 space-x-4 bg-neutral rounded-box">
+      {/* <div className="hidden  h-[28rem] lg:carousel carousel-center   p-4 space-x-4 bg-neutral rounded-box">
         {carouselImages.map((image, index) => {
           return (
             <div key={image} className="carousel-item">
@@ -38,6 +39,76 @@ const Hero = () => {
             </div>
           );
         })}
+      </div> */}
+      <div className="hidden  h-[28rem] lg:carousel carousel-center p-4 space-x-4 bg-neutral rounded-box">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={hero1} className="w-full rounded-box" />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a
+              href="#slide4"
+              className="btn btn-circle opacity-25 hover:opacity-50"
+            >
+              ❮
+            </a>
+            <a
+              href="#slide2"
+              className="btn btn-circle opacity-25 hover:opacity-50"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={hero2} className="w-full rounded-box" />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a
+              href="#slide1"
+              className="btn btn-circle opacity-25 hover:opacity-50"
+            >
+              ❮
+            </a>
+            <a
+              href="#slide3"
+              className="btn btn-circle opacity-25 hover:opacity-50"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={hero3} className="w-full rounded-box" />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a
+              href="#slide2"
+              className="btn btn-circle opacity-25 hover:opacity-50"
+            >
+              ❮
+            </a>
+            <a
+              href="#slide4"
+              className="btn btn-circle opacity-25 hover:opacity-50"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src={hero4} className="w-full rounded-box" />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a
+              href="#slide3"
+              className="btn btn-circle opacity-25 hover:opacity-50"
+            >
+              ❮
+            </a>
+            <a
+              href="#slide1"
+              className="btn btn-circle opacity-25 hover:opacity-50"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
