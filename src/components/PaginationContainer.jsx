f2c79f39b44cecef979cb55dcc3d4fa79ac2fa2c
Nemanja Stojanovic <feemail042@gmail.com>
2024-10-20 19:38:33 +0200

    Products Page init setup / check for response.meta.data

---
+++ src/components/PaginationContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PaginationContainer = () => {
+  return (
+    <div>
+      <h1>PaginationContainer</h1>
+    </div>
+  );
+};
+
+export default PaginationContainer;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-// import SingleProduct from "../components/SingleProduct";
+import Filters from "../components/Filters";
+import ProductsContainer from "../components/ProductsContainer";
+import PaginationContainer from "../components/PaginationContainer";
+
+import { useLoaderData, Link } from "react-router-dom";
+import { customFetch, formatPrice } from "../utils";
+
+export const loader = async () => {
+  const response = await customFetch("/products");
+  const products = response.data.data;
+  const meta = response.data.meta;
+  return { products, meta };
+};
 
 const Products = () => {
+  const { products, meta } = useLoaderData();
+  // console.log(meta);
+
   return (
     <div>
       <h1>Products</h1>
-      {/* <SingleProduct /> */}
+      <Filters />
+      <ProductsContainer />
+      <PaginationContainer />
     </div>
   );
 };
