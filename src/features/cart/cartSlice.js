0d370e9276ebe1237df3cb5365886691e4d625b9
Nemanja Stojanovic <feemail042@gmail.com>
2024-10-24 21:40:50 +0200

    RTK setup initial

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+
+const defaultState = {
+  cartItems: [],
+  numItemsInCart: 0,
+  cartTotal: 0,
+  shipping: 500,
+  tax: 0,
+  orderTotal: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: defaultState,
+  reducers: {
+    addCart: (state, action) => {
+      console.log(action.payload);
+    },
+    clearCart: (state) => {},
+
+    removeCart: (state, action) => {},
+    editCart: (state, action) => {},
+  },
+});
+
+export const { addCart, clearCart, removeCart, editCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,14 @@ import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
+import { store } from "./features/store.js";
+import { Provider } from "react-redux";
+
 createRoot(document.getElementById("root")).render(
   <>
-    <App />
-    <ToastContainer position="top-center" />
+    <Provider store={store}>
+      <App />
+      <ToastContainer position="top-center" />
+    </Provider>
   </>
 );
